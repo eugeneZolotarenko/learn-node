3ea1aa0f64023331cfba6c01b9311fdb9eca1f91
eugeneZolotarenko <zolotarenko.yevheniy1998@gmail.com>
2020-09-19 18:03:31 +0200

    Adding demo functionality and try pug

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
-const http = require("http")
+const path = require("path")
 
-const routes = require("./routes")
+const express = require("express")
+const bodyParser = require("body-parser")
 
-const server = http.createServer(routes)
+const rootDir = require("./utils/path")
 
-server.listen(3000)
+const app = express()
+
+app.set("view engine", "pug")
+app.set("views", "views")
+
+const adminData = require("./routes/admin")
+const shopRoutes = require("./routes/shop")
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.static(path.join(rootDir, "public")))
+
+app.use("/admin", adminData.routes)
+app.use(shopRoutes)
+
+app.use((req, res, next) => {
+  res.status(404).render("404", { pageTitle: "Page not found" })
+})
+
+app.listen(3000)
---
+++ routes/shop.js
@@ -0,0 +1,15 @@
+const path = require("path")
+
+const express = require("express")
+
+const rootDir = require("../utils/path")
+const adminData = require("./admin")
+
+const router = express.Router()
+
+router.get("/", (req, res) => {
+  const products = adminData.products
+  res.render("shop", { products, pageTitle: "Shop", path: "/" })
+})
+
+module.exports = router
