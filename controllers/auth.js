4e8a858c5ef1f2673ea71880570d63004f981d5b
eugeneZolotarenko <zolotarenko.yevheniy1998@gmail.com>
2020-11-08 20:35:59 +0100

    Adding email sending

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,12 +1,30 @@
 const User = require("../models/user")
 const bcrypt = require("bcryptjs")
+const nodemailer = require("nodemailer")
+const sendgridTransport = require("nodemailer-sendgrid-transport")
+
+const transporter = nodemailer.createTransport(
+  sendgridTransport({
+    auth: {
+      api_key: process.env.SENDGRID_KEY,
+    },
+  })
+)
 
 exports.getLogin = async (req, res, next) => {
   try {
+    let message = req.flash("error")
+    if (message.length > 0) {
+      message = message[0]
+    } else {
+      message = null
+    }
+    console.log(req.flash("error"))
     await res.render("auth/login", {
       path: "/login",
       pageTitle: "Login",
       isAuthenticated: false,
+      errorMessage: message,
     })
   } catch (err) {
     console.log(err)
@@ -19,6 +37,7 @@ exports.postLogin = async (req, res, next) => {
     req.session.isLoggedIn = true
     const user = await User.findOne({ email })
     if (!user) {
+      req.flash("error", "Invalid email or passwords")
       return await res.redirect("/login")
     }
     const doMatch = await bcrypt.compare(password, user.password)
@@ -27,6 +46,7 @@ exports.postLogin = async (req, res, next) => {
       await req.session.save()
       await res.redirect("/")
     } else {
+      req.flash("error", "Invalid email or passwords")
       await res.redirect("/login")
     }
   } catch (err) {
@@ -36,10 +56,17 @@ exports.postLogin = async (req, res, next) => {
 
 exports.getSignup = async (req, res, next) => {
   try {
+    let message = req.flash("error")
+    if (message.length > 0) {
+      message = message[0]
+    } else {
+      message = null
+    }
     await res.render("auth/signup", {
       path: "/signup",
       pageTitle: "Signup",
       isAuthenticated: false,
+      errorMessage: message,
     })
   } catch (err) {
     console.log(err)
@@ -51,6 +78,7 @@ exports.postSignup = async (req, res, next) => {
     const { email, password, confirmPassword } = req.body
     const userDoc = await User.findOne({ email })
     if (userDoc) {
+      req.flash("error", "Email already exists")
       return await res.redirect("/signup")
     }
     const hashedPassword = await bcrypt.hash(password, 12)
@@ -61,6 +89,12 @@ exports.postSignup = async (req, res, next) => {
     })
     await user.save()
     await res.redirect("/")
+    await transporter.sendMail({
+      to: email,
+      from: "zolotarenko.yevheniy1998@gmail.com",
+      subject: "Sugnup succeeded!",
+      html: "<h1>You successfully signed up!</h1>",
+    })
   } catch (err) {
     console.log(err)
   }
